df470acd4f0620755cc5c64bc55d585e0609e75c
Mauricio Tomaz <mtomaz@whiteoutsolutions.com>
2022-05-02 12:31:13 -0700

    Add bottom border in the header element

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 (() => {
   const toggleEl = document.querySelector(".navbar__toggle");
   const menuEl = document.querySelector(".navbar__menu");
+  const bodyEl = document.querySelector("body");
+  const headerEl = document.querySelector(".header");
+
   toggleEl.addEventListener("click", () =>
     menuEl.classList.toggle("navbar__menu--expanded")
   );
+
+  bodyEl.onscroll = logScroll;
+
+  setTimeout(logScroll, 0);
+
+  function logScroll() {
+    if (window.scrollY > 150) {
+      headerEl.classList.add("header--with-border");
+    } else {
+      headerEl.classList.remove("header--with-border");
+    }
+  }
 })();
